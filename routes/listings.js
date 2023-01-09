e6dd22711fe2687770ec1aa2548016cbcfb7d235
Timothy <toqul4j@gmail.com>
2023-01-09 15:51:09 -0800

    add listings page frontend

--- routes/listings.js
+++ routes/listings.js
@@ -1,8 +1,24 @@
 var express = require("express");
 var router = express.Router();
 
+const listings = [
+    {
+        title: "kitten",
+        location: "Rowland Heights",
+        image: "images/rabbits.jpg",
+    },
+    {
+        title: "Cute lionhead Baby Bunny Rabbits",
+        location: "Glendale",
+    },
+    {
+        title: "Cute lionhead Baby Bunny Rabbits",
+        location: "Glendale",
+    },
+];
+
 router.get("/", function (req, res) {
-    res.render("listings", { title: "listings" });
+    res.render("listings", { title: "listings", listings: listings });
 });
 
 module.exports = router;
